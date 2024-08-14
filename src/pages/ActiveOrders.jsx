9dcc6309338ed91a942d49b4dba5774e6feee505
fulpopy <akash16799@gmail.com>
2024-08-14 22:20:29 +0530

    added MUI and header

--- src/pages/ActiveOrders.jsx
+++ src/pages/ActiveOrders.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../components";
 
 const ActiveOrders = () => {
   return (
-    <div>ActiveOrders</div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default ActiveOrders
+export default ActiveOrders;
